bbb5418bb92af812eea42fcf5c7692b9f9a34c98
octanym <camjmoore@outlook.com>
2023-07-04 11:53:00 -0400

    imported project schema to sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import project from "./sanity/schemas/project-schema";
 
 const config = defineConfig({
   projectId: "s7ponpx3",
@@ -7,7 +8,8 @@ const config = defineConfig({
   title: "cjmoore",
   apiVersion: "2023-02-07",
   basePath: "/admin",
-  plugins: [deskTool()]
-})
+  plugins: [deskTool()],
+  schema: { types: [project] },
+});
 
 export default config;
