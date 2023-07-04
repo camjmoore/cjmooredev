3b1f712e455e8eeae534c1650a3ae7a1187abc85
octanym <camjmoore@outlook.com>
2023-07-04 17:10:18 -0400

    implemented first groq query

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,4 +1,4 @@
-import { createClient } from "next-sanity";
+import { createClient, groq } from "next-sanity";
 
 export async function getProjects() {
   const client = createClient({
@@ -6,4 +6,26 @@ export async function getProjects() {
     dataset: "production",
     apiVersion: "2023-02-07",
   });
+
+  //for all types that match projects
+  //return
+  //id
+  //created stamp
+  //name
+  //slug.current as slug
+  //image url destructured from image.asset as image
+  //url
+  //content
+
+  return client.fetch(
+    groq`*[_type == "project" ]{
+      _id,
+      _createdAt
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`
+  );
 }
