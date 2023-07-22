e09ca98eaeb36bccedea730dcb1cfc613e18a41a
octanym <camjmoore@outlook.com>
2023-07-22 18:15:11 -0400

    moved page from /app to /projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { getProjects } from "../sanity/sanity-utils";
-
 export default async function Home() {
-  //instead of using getStaticPaths and getStaticProps to run getProjects and handle the data
-  //now in next13 you can just do this
-
-  const projects = await getProjects();
-
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
+    <div className="flex justify-center">
+      <h1 className="text-7xl text-white">Home page</h1>
     </div>
   );
 }
---
+++ app/projects/page.tsx
@@ -0,0 +1,16 @@
+import { getProjects } from "../../sanity/sanity-utils";
+
+export default async function Home() {
+  //instead of using getStaticPaths and getStaticProps to run getProjects and handle the data
+  //now in next13 you can just do this
+
+  const projects = await getProjects();
+
+  return (
+    <div>
+      {projects.map((project) => (
+        <div key={project._id}>{project.name}</div>
+      ))}
+    </div>
+  );
+}
