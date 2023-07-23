5fc5e251a95f849ea770c0e4f55f43773b935ad8
octanym <camjmoore@outlook.com>
2023-07-23 16:41:15 -0400

    posts mapped and linked

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,15 @@
 import { getPosts } from "../../sanity/sanity-utils";
+import Link from "next/link";
 
 export default async function Posts() {
   const posts = await getPosts();
 
   return (
     <div>
-      {posts.map((posts) => (
-        <div key={posts._id}>{posts.name}</div>
+      {posts.map((post) => (
+        <Link href={`/posts/${post.slug}`} key={post._id}>
+          {post.name}
+        </Link>
       ))}
     </div>
   );
