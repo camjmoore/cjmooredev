c93051ba7f7f38bb95eaedb1de2ec1056633f1fa
octanym <camjmoore@outlook.com>
2023-07-23 19:08:01 -0400

    moved components and routes into corresponding organizational folders

--- app/admin/[[...index]]/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import config from "@/sanity.config"; //available as an alias in root of project
-import { NextStudio } from "next-sanity/studio";
-
-export default function AdminPage() {
-  return <NextStudio config={config} />;
-}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Home() {
-  return (
-    <div className="flex justify-center">
-      <h1 className="text-7xl text-white">Home page</h1>
-    </div>
-  );
-}
