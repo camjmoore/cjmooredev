231cd738874abbc5878ce67410970c6ad346141a
octanym <camjmoore@outlook.com>
2023-07-23 16:46:22 -0400

    display project content with PortableText component

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,8 +1,20 @@
+import { getProject } from "../../../sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+
 type Props = { params: { project: string } };
 
 export default async function Project({ params }: Props) {
   const slug = params.project;
   const project = await getProject(slug);
 
-  return <div>This Project</div>;
+  return (
+    <div>
+      <header>
+        <h1>{project.name}</h1>
+      </header>
+      <div>
+        <PortableText value={project.content} />
+      </div>
+    </div>
+  );
 }
