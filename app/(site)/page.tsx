4326c91cea8e7f07b45b7a18e72b7fa1fb017906
octanym <camjmoore@outlook.com>
2023-07-23 19:05:03 -0400

    moved site related routes and components to (site)

---
+++ app/(site)/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-7xl text-white">Home page</h1>
+    </div>
+  );
+}
---
+++ app/(site)/posts/page.tsx
@@ -0,0 +1,16 @@
+import { getPosts } from "@/sanity/sanity-utils";
+import Link from "next/link";
+
+export default async function Posts() {
+  const posts = await getPosts();
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <Link href={`/posts/${post.slug}`} key={post._id}>
+          {post.name}
+        </Link>
+      ))}
+    </div>
+  );
+}
