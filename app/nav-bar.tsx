36edc5beb17c1f53ab1ae526b7a6047ac516f2e5
octanym <camjmoore@outlook.com>
2023-07-23 22:38:45 -0400

    simplified and refactored nav component using usePathname hook

--- app/nav-bar.tsx
+++ app/nav-bar.tsx
@@ -1,107 +1,44 @@
 "use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { useState } from "react";
 
-const MENU_VALUES = {
-  about: false,
-  projects: false,
-  posts: false,
-  contact: false,
-};
+export default function Nav() {
+  const pathname = usePathname();
 
-export default function HeadlessNav() {
-  const [open, setOpen] = useState(MENU_VALUES);
-
-  const toggleOpen = (e) => {
-    setOpen({ ...MENU_VALUES, [e.target.name]: !e.target.value });
-    console.log(e.target.name);
-  };
+  const menu = [
+    { name: "about", href: "/about" },
+    { name: "projects", href: "/projects" },
+    { name: "posts", href: "/posts" },
+    { name: "contact", href: "/contact" },
+  ];
 
   return (
     <div className="flex flex-1 items-center justify-center h-20 max-w-full mx-auto">
-      <nav className="pointer-events-auto md:block">
-        <ul className="flex rounded-full px-3 text-sm bg-slate-900 text-zinc-200 ring-1 ring-white/10">
-          <Link
-            href="/about"
-            name="about"
-            value={open.about}
-            onClick={toggleOpen}
-            className={
-              open.about
-                ? "relative block px-3 py-2 transition text-orange-400"
-                : "relative block px-3 py-2 transition hover:text-orange-400"
-            }
-          >
-            About
-            <span
-              className={`${
-                open.about
-                  ? "absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-orange-500/0 via-orange-500/40 to-orange-500/0 dark:from-orange-400/0 dark:via-orange-400/40 dark:to-orange-400/0"
-                  : "hidden"
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/projects"
-            name="projects"
-            value={open.projects}
-            onClick={toggleOpen}
-            className={
-              open.projects
-                ? "relative block px-3 py-2 transition text-orange-400"
-                : "relative block px-3 py-2 transition hover:text-orange-400"
-            }
-          >
-            Projects
-            <span
-              className={`${
-                open.projects
-                  ? "absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-orange-500/0 via-orange-500/40 to-orange-500/0 dark:from-orange-400/0 dark:via-orange-400/40 dark:to-orange-400/0"
-                  : "hidden"
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/posts"
-            name="posts"
-            value={open.posts}
-            onClick={toggleOpen}
-            className={
-              open.posts
-                ? "relative block px-3 py-2 transition text-orange-400"
-                : "relative block px-3 py-2 transition hover:text-orange-400"
-            }
-          >
-            Posts
-            <span
-              className={`${
-                open.posts
-                  ? "absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-orange-500/0 via-orange-500/40 to-orange-500/0 dark:from-orange-400/0 dark:via-orange-400/40 dark:to-orange-400/0"
-                  : "hidden"
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/contact"
-            name="contact"
-            value={open.contact}
-            onClick={toggleOpen}
-            className={
-              open.contact
-                ? "relative block px-3 py-2 transition text-orange-400"
-                : "relative block px-3 py-2 transition hover:text-orange-400"
-            }
-          >
-            Contact
-            <span
-              className={`${
-                open.contact
-                  ? "absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-orange-500/0 via-orange-500/40 to-orange-500/0 dark:from-orange-400/0 dark:via-orange-400/40 dark:to-orange-400/0"
-                  : "hidden"
-              }`}
-            ></span>
-          </Link>
-        </ul>
+      <nav className="flex rounded-full px-3 text-sm bg-slate-900 text-zinc-200 ring-1 ring-white/10 pointer-events-auto">
+        {menu.map((link) => {
+          const isActive = pathname.startsWith(link.href);
+
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              className={
+                isActive
+                  ? "relative block px-3 py-2 transition text-orange-400"
+                  : "relative block px-3 py-2 transition hover:text-orange-400"
+              }
+            >
+              {link.name}
+              <span
+                className={`${
+                  isActive
+                    ? "absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-orange-500/0 via-orange-500/40 to-orange-500/0 dark:from-orange-400/0 dark:via-orange-400/40 dark:to-orange-400/0"
+                    : "hidden"
+                }`}
+              ></span>
+            </Link>
+          );
+        })}
       </nav>
     </div>
   );
