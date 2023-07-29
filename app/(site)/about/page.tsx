32aa009482c158f4d0a78b31be26972c48b693d0
octanym <camjmoore@outlook.com>
2023-07-28 23:13:33 -0400

    fixed responsive resizing issue

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -2,25 +2,23 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="mx-auto w-[50rem]">
-      <section className="flex justify-evenly items-center">
-        <div>
-          <h2 className="text-3xl text-white"> Cameron J Moore </h2>
-          <p className="text-5md text-white">
-            {" "}
-            Front-End Developer | Full-Stack Interloper
-          </p>
-        </div>
-        <div className="h-24 w-24 overflow-hidden items-center rounded-full border border-zinc-400">
-          <Image
-            src="/headshot.png"
-            alt="a headshot photo of the next dev you plan to hire"
-            quality={50}
-            width={100}
-            height={100}
-          />
-        </div>
-      </section>
+    <div>
+      <div>
+        <h2 className="text-3xl text-white"> Cameron J Moore </h2>
+        <p className="text-5md text-white">
+          {" "}
+          Front-End Developer | Full-Stack Interloper
+        </p>
+      </div>
+      <div className="h-24 w-24 overflow-hidden items-center rounded-full border border-zinc-400">
+        <Image
+          src="/headshot.png"
+          alt="a headshot photo of the next dev you plan to hire"
+          quality={50}
+          width={100}
+          height={100}
+        />
+      </div>
     </div>
   );
 }
