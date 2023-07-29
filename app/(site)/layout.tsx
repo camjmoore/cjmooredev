9d962288d047a07ddc21d60127da41112026d744
octanym <camjmoore@outlook.com>
2023-07-28 23:32:03 -0400

    layout responsive styling

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <Nav />
         <Header />
         <div className="sm:px-8">
-          <div className="mx-auto max-w-7xl lg:px-12">
+          <div className="mx-auto max-w-5xl lg:px-12">
             <div className="relative px-4 sm:px-8 lg:px-12">
               <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
             </div>
