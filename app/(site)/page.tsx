1a672721d52e3149b3a06ec9492136e4d64cfa72
octanym <camjmoore@outlook.com>
2023-07-31 23:44:14 -0400

    updated placeholder

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <div className="flex justify-center">
-      <h1 className="text-7xl text-white">Home page</h1>
+      <h1>Home page</h1>
     </div>
   );
 }
