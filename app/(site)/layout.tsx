f7a2c72e5f5820e4117e72a96f2d434eebfde47d
octanym <camjmoore@outlook.com>
2023-07-31 23:40:22 -0400

    installed next fonts and imported local quattro font

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,5 @@
 import "../globals.css";
+import localFont from "@next/font/local";
 import Header from "../header";
 import Nav from "../nav-bar";
 
@@ -7,6 +8,31 @@ export const metadata = {
   description: "personal portfolio and blog generated with create next app",
 };
 
+const quattro = localFont({
+  src: [
+    {
+      path: "../fonts/iAWriterQuattroS-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/iAWriterQuattroS-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../fonts/iAWriterQuattroS-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/iAWriterQuattroS-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -14,7 +40,7 @@ export default function RootLayout({
 }) {
   return (
     <html
-      className="bg-gradient-to-b from-[#001B26] to-slate-900 opacity-95 min-h-screen"
+      className={`${quattro.className} bg-gradient-to-b from-bathys1 to-abyss1 min-h-screen`}
       lang="en"
     >
       <body className="bg-no-repeat bg-spectrum bg-top min-h-screen">
@@ -23,7 +49,9 @@ export default function RootLayout({
         <div className="sm:px-8">
           <div className="mx-auto max-w-5xl lg:px-12">
             <div className="relative px-4 sm:px-8 lg:px-12">
-              <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+              <div className="mx-auto mb-12 max-w-2xl lg:max-w-5xl">
+                {children}
+              </div>
             </div>
           </div>
         </div>
