ce5c51e324cbdd3cec06ec9dd3a3ea7afda19b16
octanym <camjmoore@outlook.com>
2023-07-31 23:38:23 -0400

    styled projects, theme colors and spacing

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -9,26 +9,29 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-3">
-      {projects.map((project) => (
-        <Link
-          href={`/projects/${project.slug}`}
-          key={project._id}
-          className="mx-auto w-60 h-72 overflow-hidden rounded-lg border border-gray-500"
-        >
-          {project.image && (
-            <div className="max-w-fit h-52 overflow-hidden">
-              <Image
-                src={project.image}
-                alt={project.name}
-                width={250}
-                height={200}
-              />
-            </div>
-          )}
-          <div>{project.name}</div>
-        </Link>
-      ))}
+    <div>
+      <h1 className="self-start pl-1">Projects</h1>
+      <div className="grid grid-cols-1 gap-12 mt-8 sm:grid-cols-2 lg:grid-cols-3">
+        {projects.map((project) => (
+          <Link
+            href={`/projects/${project.slug}`}
+            key={project._id}
+            className="mx-auto w-60 h-72 overflow-hidden rounded-lg border border-nimbo2"
+          >
+            {project.image && (
+              <div className="max-w-fit h-52 overflow-hidden">
+                <Image
+                  src={project.image}
+                  alt={project.name}
+                  width={250}
+                  height={200}
+                />
+              </div>
+            )}
+            <h2 className="text-center mt-6">{project.name}</h2>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
